f30be5b097bdb4b884a2f86dcaa8e13b753faec5
ez <ezweave@gmail.com>
2021-06-11 10:08:11 -0600

    moving quick

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import corsLogo from './CORS.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+const App = () =>
+(
+  <div className="App">
+    <header className="App-header">
+      <img src={corsLogo} className="App-logo" alt="logo" />
+      <p>
+        CORS Tester.  Do it light.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <form>
+        <label>
+          Name:
+      <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </header>
+  </div>
+);
 
 export default App;
